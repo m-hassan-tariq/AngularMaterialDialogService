1060a28ff5f6ec3f43bcc4e381d5f70e44cc108f
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-02-26 15:51:27 -0600

    Initial

---
+++ Scripts/app/app.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngMaterial']);
+
+    app.controller('dialogController', function (confirmAlert) {
+
+        var vm = this;
+        
+        vm.confirm= function () {
+            confirmAlert.confirmBoxResponse(
+                'Would you like to delete?',
+                'Record will be deleted ',
+                'Delete',
+                callBack,
+                "Hello World");
+        };
+
+        function callBack(param) {
+            vm.result = param;
+        }
+
+    });
+
+})();
